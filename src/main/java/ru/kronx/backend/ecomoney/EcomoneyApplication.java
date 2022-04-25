6c6b101475734d9a52c77f06a0ba0214616396fe
Kirill <pak.kiruha@yandex.ru>
2022-04-26 01:00:31 +0300

    init start project

---
+++ src/main/java/ru/kronx/backend/ecomoney/EcomoneyApplication.java
@@ -0,0 +1,13 @@
+package ru.kronx.backend.ecomoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomoneyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kronx/backend/ecomoney/EcomoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kronx.backend.ecomoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomoneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
